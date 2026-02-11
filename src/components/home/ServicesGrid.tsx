0d4d6304f7c11d0d174f819e8f11e25eb745482c
rankandrent <102636183+rankandrent@users.noreply.github.com>
2026-02-11 19:53:37 +0500

    UI: Update homepage services grid to 3 columns

--- src/components/home/ServicesGrid.tsx
+++ src/components/home/ServicesGrid.tsx
@@ -19,7 +19,7 @@ export function ServicesGrid() {
                     </P>
                 </div>
 
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
                     {displayedServices.map((service) => (
                         <Link
                             key={service.slug}
